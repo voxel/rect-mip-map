7f760d847a9e262e3c06f9baafaaffc340cc5070
deathcap <deathcap@gmx.co.uk>
2014-04-12 13:13:29 -0700

    Tilepad beforehand in demo, to 2x2, since atlaspack tilepad is 1/2 x 1/2

--- demo.js
+++ demo.js
@@ -5,10 +5,12 @@ var ndarray = require('ndarray');
 var getPixels = require('get-pixels');
 var savePixels = require('save-pixels');
 var rectMipMap = require('./mipmap-r.js');
+var touchup = require('touchup');
 
 var canvas = document.createElement('canvas');
 
 var SIZE = 512;
+var TILEPAD = 2;
 
 // TODO: remove dependence on canvas
 canvas.width = canvas.height = SIZE;
@@ -17,6 +19,7 @@ canvas.style.border = '1px solid black';
 //document.body.appendChild(canvas); // redundant since we also show mip level #0
 
 var atlas = createAtlas(canvas);
+global.atlas = atlas; // debug
 
 var imageURLs = {
   // test 16x16 images
@@ -28,34 +31,52 @@ var imageURLs = {
   crate: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAYAAADDPmHLAAABvUlEQVR42u3YsQ2DUBAFwX/I3dCEm6BfmqAe0wG52XkpGTs6CeZY81uW3eYVAGAAGAAGgAFgsX2eHu5fX4j/suscF0B8AMQHQHwAxAdAfADEB0B8AMQHQHwA0vGf/ugCEI4PQDw+APH4AMTjAxCPD0A8PgDx+ADE4wMQjw9APD4A8fgAxOMDEI8PQDw+APH4AMTjAxCPD0A8PgDx+ADE4wMQjw9APD4A8fgAxOMDEI8PQDw+APH4AMTjAxCPD0A8PgDx+ADE4wMQjw9APD4A8fgAxOMDEI8PQDw+APH4AMTj5wHU46cBiB8GIH4YgPhhAOKHAYgfBiB+GID4YQDihwGIHwYgfhiA+GEA4ocBiB8GIH4YgPhhAOKHAYgfBiB+GID4YQDihwGIHwYgfhiA+GEA4ocBiB8GIH4YgPhhAOKHAYgfBiB+GID4YQDihwGIHwYgfhiA+GEA4ocBiB8GIH4YgPhhAOKHAYgfBiB+GID4YQDihwGIHwYgfhiA+GEA4ocBiB8GIH4YgPhhAOKHAYgfBiB+GID4YQDiuwDiAyA+AOIDID4A4gMgPgDiv2pzrFHPBTAADAADwACw0m7+YETmqldkNQAAAABJRU5ErkJggg=='
 };
 
-var getImg = function(name) {
+var getImg = function(name, cb) {
   var img = new Image();
+  img.onload = function() {
+    var img2 = new Image();
+    img2.src = touchup.repeat(img, TILEPAD, TILEPAD);
+    img2.name = name;
+    cb(null, img2);
+  };
+  img.onerror = function(err) {
+    cb(err);
+  };
   img.src = imageURLs[name];
   img.name = name;
-  return img;
 };
 
-['grass', 'stone', 'crate', 'stick'].forEach(function(name) {
-  atlas.pack(getImg(name));
-});
-//atlas.pack(getImg(images.logo));
+var generate = function() {
+  //atlas._debug(); // shows red borders around each rect
+  getPixels(canvas.toDataURL(), function(err, array) {
+    if (err) throw new Error('get-pixels failed: '+err);
 
-//atlas._debug(); // shows red borders around each rect
-global.atlas = atlas; // debug
+    var pyramid = rectMipMap(array, atlas);
+    console.log(pyramid);
 
-getPixels(canvas.toDataURL(), function(err, array) {
-  if (err) throw new Error('get-pixels failed: '+err);
+    pyramid.forEach(function(level, i) {
+      var img = new Image();
+      img.src = savePixels(level, 'canvas').toDataURL();
+      img.style.border = '1px dotted black';
+      document.body.appendChild(document.createElement('br'));
+      document.body.appendChild(img);
+      document.body.appendChild(document.createTextNode(' level #'+i+' ('+img.width+'x'+img.height+')'));
+    });
+  });
+};
 
-  var pyramid = rectMipMap(array, atlas);
-  console.log(pyramid);
+var names = ['grass', 'stone', 'crate', 'stick'];
+var i = 0;
+names.forEach(function(name) {
+  getImg(name, function(err, img) {
+    if (img) atlas.pack(img);
+    if (err) throw new Error(err);
 
-  pyramid.forEach(function(level, i) {
-    var img = new Image();
-    img.src = savePixels(level, 'canvas').toDataURL();
-    img.style.border = '1px dotted black';
-    document.body.appendChild(document.createElement('br'));
-    document.body.appendChild(img);
-    document.body.appendChild(document.createTextNode(' level #'+i+' ('+img.width+'x'+img.height+')'));
+    i += 1;
+    if (i === names.length) {
+      // images finished loading, so generate mipmaps
+      generate();
+    }
   });
 });
 
