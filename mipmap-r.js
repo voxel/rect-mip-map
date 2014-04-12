4cd122a05247f9e89aef28e026d964ba840d35c7
deathcap <deathcap@gmx.co.uk>
2014-04-11 20:57:49 -0700

    Downsample per-channel

--- mipmap-r.js
+++ mipmap-r.js
@@ -70,11 +70,23 @@ var makeMipMaps = function(array, pad, rects) {
 
       for (var i = 0; i < rectsX.length; i += 1) {
         var rx = rectsX[i], ry = rectsY[i], rw = rectsW[i], rh = rectsH[i];
-        downsample(level.lo(ry,rx).hi(rh,rw), plevel.lo(ry>>1,rx>>1).hi(rh>>1,rw>>1), 0, 255)
+
+        for (var k = 0; k < channels; k += 1) {
+          var levelChannel = level.pick(undefined, undefined, k);
+          var plevelChannel = plevel.pick(undefined, undefined, k);
+
+          downsample(levelChannel.lo(ry,rx).hi(rh,rw), plevelChannel.lo(ry>>1,rx>>1).hi(rh>>1,rw>>1), 0, 255)
+        }
         //downsample(level.lo(ry,rx).hi(rh,rw), plevel.lo(ry,rx).hi(rh,rw), 0, 255)
       }
       //ops.assign(level, plevel);
-      downsample(level, plevel, 0, 255);
+      /* the all-together native downsampling that causes blending artifacts
+       */
+      for (var k = 0; k < channels; k += 1) {
+        var levelChannel = level.pick(undefined, undefined, k);
+        var plevelChannel = plevel.pick(undefined, undefined, k);
+        downsample(levelChannel, plevelChannel, 0, 255);
+      }
     }
 
     levels.push(level);
